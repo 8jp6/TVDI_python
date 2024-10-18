4bda392b6bc86231fe91aab6a561aab2aaddc1bd
8_jp6 <whps960025@gmail.com>
2024-10-18 11:21:51 +0800

    1

--- 1018_L4/lesson4_2.py
+++ 1018_L4/lesson4_2.py
@@ -3,4 +3,13 @@ root = tk.Tk()
 print(type(root))
 root.title('這是我的第一個視窗!')
 root.geometry('800x300')
+message = tk.Label(root,text='Hello! 這是我的第一個視窗!')
+message.pack()
 root.mainloop()
+
+
+
+
+'''
+widget是什麼 module class method attribute property
+'''
