d3c7c5106272eaa42bcd6a918aa9796a3d790e90
8_jp6 <whps960025@gmail.com>
2024-10-21 11:11:12 +0800

    1

--- 1021_L5/lesson5_0.py
+++ 1021_L5/lesson5_0.py
@@ -14,7 +14,7 @@ class Window(ThemedTk):
         style.configure('Main4.TButton',font=("Arial",15,"overstrike"),foreground="black")
         #================================start topframe====================================
         topFrame = ttk.Frame(self,borderwidth=1,relief='groove')
-        self.btn1 = ttk.Button(topFrame,text="加入購物車",style='Main.TButton',command=self.user_click1)
+        self.btn1 = ttk.Button(topFrame,text="加入購物車",style='Main.TButton',command=self.user_click1)#加上self.變成attribute
         self.btn1.pack(side='left',expand=True,fill='both',padx=10)
         btn2 = ttk.Button(topFrame,text="我的最愛",style='Main.TButton',command=self.user_click2)
         btn2.pack(side='left',expand=True,fill='both')
@@ -28,12 +28,15 @@ class Window(ThemedTk):
       
         #================================start leftframe================================
         leftFrame =ttk.Frame(bottomFrame,width=100,height=300,borderwidth=2,relief='groove')
-        btn1 = ttk.Button(leftFrame,text="Home",style='Main1.TButton')
-        btn1.pack(expand=True,fill='x',padx=10,pady=5,ipady=50)
-        btn2 = ttk.Button(leftFrame,text="Cancel",style='Main2.TButton')
-        btn2.pack(expand=True,fill='x',padx=10,pady=5,ipady=25)
-        btn3 = ttk.Button(leftFrame,text="Chat",style='Main1.TButton')
-        btn3.pack(expand=True,fill='x',padx=10,pady=5,ipady=25)
+        btn4 = ttk.Button(leftFrame,text="Home",style='Main1.TButton')
+        btn4.bind('<ButtonRelease>',self.left_button_click)
+        btn4.pack(expand=True,fill='x',padx=10,pady=5,ipady=50)
+        btn5 = ttk.Button(leftFrame,text="Cancel",style='Main2.TButton')
+        btn5.bind('<ButtonRelease>',self.left_button_click)
+        btn5.pack(expand=True,fill='x',padx=10,pady=5,ipady=25)
+        btn6 = ttk.Button(leftFrame,text="Chat",style='Main1.TButton')
+        btn6.bind('<ButtonRelease>',self.left_button_click)
+        btn6.pack(expand=True,fill='x',padx=10,pady=5,ipady=25)
         leftFrame.pack(padx=10,pady=10,side="left",expand=True,fill='both')
         #================================end leftframe================================
 
@@ -62,7 +65,7 @@ class Window(ThemedTk):
         #================================end bottomframe==================================
 
     def user_click1(self):
-        self.btn1.configure(text="被按了")
+        self.btn1.configure(text="被按了")#self.呼叫attribute
         print("Hello!button1")
 
     def user_click2(self):
@@ -70,6 +73,13 @@ class Window(ThemedTk):
 
     def user_click3(self):
         print("Hello1 button3")
+
+    def left_button_click(self,event):
+        print(type(event))
+        print(event.x)
+        print(event.y)
+        print(event.width)
+        print(event.widget.configure(text="被按了"))
 
 def main():
     window = Window(theme="arc")
