b060327adddcbb190cfaedcece035ccb1b621c7f
8_jp6 <whps960025@gmail.com>
2024-10-21 16:13:08 +0800

    1

--- 1021_L5/lesson5_1.py
+++ 1021_L5/lesson5_1.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
+from tkinter.messagebox import showinfo
 
 class Window(ThemedTk):
     def __init__(self, *args, **kwargs):
@@ -20,21 +21,32 @@ class Window(ThemedTk):
         bottomFrame = ttk.Frame(self)
         bottomFrame.columnconfigure(index= 0, weight=1)
 
-        ttk.Label(bottomFrame, text="輸入姓名").grid(column= 0 ,row= 0,padx= 10,pady = 10)
+        ttk.Label(bottomFrame, text="輸入帳號").grid(column= 0 ,row= 0,padx= 10,pady = 10)
         self.usn_entry=tk.StringVar()
-        ttk.Entry(bottomFrame).grid (column = 1,row = 0,padx= 10,pady = 10)
+        ttk.Entry(bottomFrame,textvariable=self.usn_entry).grid (column = 1,row = 0,padx= 10,pady = 10)
+
         ttk.Label(bottomFrame, text='輸入密碼').grid(column= 0, row= 1,padx= 10, pady= 10)
         self.pw_entry=tk.StringVar()
-        ttk.Entry(bottomFrame).grid(column= 1, row= 1,padx= 10, pady= 10)
-
-
-        ok_btn = ttk.Button(bottomFrame,text='確定')
-        ok_btn.grid(column=0, row=2,padx=10,pady=(30,0))
-        cancel_btn = ttk.Button(bottomFrame,text='取消')
-        cancel_btn.grid(column=1,row=2,padx=10,pady=(30,0),sticky='E')
+        ttk.Entry(bottomFrame,textvariable=self.pw_entry,show='@').grid(column= 1, row= 1,padx= 10, pady= 10)
+        
+        ok_btn = ttk.Button(bottomFrame,text='確定', command=self.ok_click)
+        ok_btn.grid(column=0, row=3,padx=10,pady=(30,0))
+        cancel_btn = ttk.Button(bottomFrame,text='取消',command=self.cancer_click)
+        cancel_btn.grid(column=1,row=3,padx=10,pady=(30,0),sticky='E')
 
         bottomFrame.pack(expand=True,fill='x',padx= 10,pady= 10)
         #end下盒
+
+    def cancer_click(self):
+        self.usn_entry.set('')
+        self.pw_entry.set('')
+
+    def ok_click(self):
+        username = self.usn_entry.get()
+        password = self.pw_entry.get()
+        showinfo(title="使用者輸入",message=f'使用者名稱:{username}\n使用者密碼:{password}')
+
+
 
         # bottombottomframe = ttk.Frame(self)
         # okbut = ttk.Button(bottombottomframe,text='確認')
@@ -47,8 +59,8 @@ class Window(ThemedTk):
 
 def main():
     window = Window(theme="arc")
-    window.usn_entry.set("account")
-    window.pw_entry.set("password")
+    # window.usn_entry.set("account")
+    # window.pw_entry.set("password")
     window.mainloop()
 
 if __name__ == '__main__':
