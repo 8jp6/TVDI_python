4530c635a544529a4f44600f71369b72c520e90d
8_jp6 <whps960025@gmail.com>
2024-11-04 14:05:23 +0800

    1

--- 1104_L7/Lesson7_0.py
+++ 1104_L7/Lesson7_0.py
@@ -1,4 +1,4 @@
-import datasource
+import L7datasource
 from tkinter import ttk
 import tkinter as tk
 from ttkthemes import ThemedTk
@@ -23,7 +23,7 @@ class Window(ThemedTk):
 
         #==============bottomFrame===============
         bottomFrame = ttk.Frame(self)
-        sitenames = datasource.get_sitename()
+        sitenames = L7datasource.get_sitename()
         self.selected_site = tk.StringVar()
         print(sitenames)
 
@@ -33,7 +33,7 @@ class Window(ThemedTk):
         sitenames_cb.bind('<<ComboboxSelected>>',self.sitename_selected)
         sitenames_cb.pack(side='left',expand=True,anchor='n')
 
-        columns = ('date', 'country','aqi','pm2.5','status','lat','long')
+        columns = ('date', 'country','aqi','pm2.5','status','long','lat')
         self.tree = ttk.Treeview(bottomFrame,columns=columns, show='headings')
 
         self.tree.heading('date', text='日期')
@@ -41,24 +41,17 @@ class Window(ThemedTk):
         self.tree.heading('aqi', text='AQI')
         self.tree.heading('pm2.5', text='pm2.5')
         self.tree.heading('status', text='狀態')
-        self.tree.heading('lat', text='緯度')
         self.tree.heading('long', text='經度')
+        self.tree.heading('lat', text='緯度')
         #ctrl+F2一次全選同樣的字
         self.tree.column('date', width=120,anchor='center')
         self.tree.column('country', width=100,anchor='center')
         self.tree.column('aqi', width=50,anchor='center')
         self.tree.column('pm2.5', width=50,anchor='center')
         self.tree.column('status', width=50,anchor='center')
-        self.tree.column('lat', width=100,anchor='center')
         self.tree.column('long', width=100,anchor='center')
-        
+        self.tree.column('lat', width=100,anchor='center')
 
-        # contacts=[]
-        # for n in range(1,100):
-        #     contacts.append((f'first{n}',f'last{n}',f'email{n}example.com'))
-
-        # for contact in contacts:
-        #     tree.insert('',tk.END,values=contact)
 
         self.tree.pack(side='right')
         bottomFrame.pack(expand=True,fill='x',padx=20,pady=(0,20),ipadx=10,ipady=10)
@@ -66,7 +59,7 @@ class Window(ThemedTk):
     
     def sitename_selected(self,event):
         selected = self.selected_site.get()
-        selected_data = datasource.get_selected_data(selected)
+        selected_data = L7datasource.get_selected_data(selected)
         for record in selected_data:
             self.tree.insert('','end',values=record)
 
@@ -74,9 +67,12 @@ class Window(ThemedTk):
 
 
     
-        
+def download():
+    L7datasource.get_all_data()
+    pass
 
 def main():
+    download()
     window = Window(theme="arc")
     window.mainloop()
 
