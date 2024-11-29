2f98ded22f5687efc5cc5f330b2fcedbf05d23db
8jp6 <whps960025@gmail.com>
2024-11-29 23:12:28 +0800

    test

--- testing/test1.py
+++ testing/test1.py
@@ -12,14 +12,35 @@ def get_exe_dir():
 
 # 設定檔案路徑
 current_dir = get_exe_dir()
-file_path = os.path.join(current_dir, '股票.csv')
+file_path = os.path.join(current_dir, '每月營收.csv')
+
+custom_fieldnames = {
+    '公司代號': '公司代號',
+    '公司名稱': '公司名稱',
+    '出表日期': '出表日期',
+    '營業收入-上月比較增減(%)': '上月比較增減(%)',
+    '營業收入-上月營收': '上月營收',
+    '營業收入-去年同月增減(%)': '去年同月增減(%)',
+    '營業收入-去年當月營收': '去年當月營收',
+    '營業收入-當月營收': '當月營收',
+    '產業別': '產業別',
+    '累計營業收入-前期比較增減(%)': '累計營業收入-前期比較增減(%)',
+    '累計營業收入-去年累計營收': '累計營業收入-去年累計營收',
+    '累計營業收入-當月累計營收': '累計營業收入-當月累計營收',
+    '資料年月': '資料年月',
+    '備註': '備註'
+}
+
 
 url = 'https://openapi.twse.com.tw/v1/opendata/t187ap05_L'
 data = requests.get(url)
 dj = data.json()
 with open(file_path,'w',encoding='utf-8-sig',newline='') as f:
     fieldnames = ['公司代號','公司名稱','出表日期','營業收入-上月比較增減(%)','營業收入-上月營收','營業收入-去年同月增減(%)','營業收入-去年當月營收','營業收入-當月營收','產業別','累計營業收入-前期比較增減(%)','累計營業收入-去年累計營收','累計營業收入-當月累計營收','資料年月','備註']
-    dict_writer = csv.DictWriter(f,fieldnames=fieldnames)
+    custom_fieldnames_list = list(custom_fieldnames.values())
+    dict_writer = csv.DictWriter(f,fieldnames=custom_fieldnames_list)
     dict_writer.writeheader()
-    for i in dj:
-        dict_writer.writerow(i)
+    for row in dj:
+        # 根據自訂欄位名稱對應值
+        custom_row = {custom_fieldnames[key]: row[key] for key in fieldnames}
+        dict_writer.writerow(custom_row)
