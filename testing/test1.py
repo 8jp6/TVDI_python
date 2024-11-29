1b863a8d2f6587b2d7bc56cccda8114d1bff4265
8jp6 <whps960025@gmail.com>
2024-11-29 23:02:18 +0800

    test

--- testing/test1.py
+++ testing/test1.py
@@ -17,7 +17,7 @@ file_path = os.path.join(current_dir, '股票.csv')
 url = 'https://openapi.twse.com.tw/v1/opendata/t187ap05_L'
 data = requests.get(url)
 dj = data.json()
-with open(file_path,'w',encoding='utf-8',newline='') as f:
+with open(file_path,'w',encoding='utf-8-sig',newline='') as f:
     fieldnames = ['公司代號','公司名稱','出表日期','營業收入-上月比較增減(%)','營業收入-上月營收','營業收入-去年同月增減(%)','營業收入-去年當月營收','營業收入-當月營收','產業別','累計營業收入-前期比較增減(%)','累計營業收入-去年累計營收','累計營業收入-當月累計營收','資料年月','備註']
     dict_writer = csv.DictWriter(f,fieldnames=fieldnames)
     dict_writer.writeheader()
