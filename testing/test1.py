2a5d5665cd43fad4a1c0b88f6d69cfe0f3070c6e
8jp6 <whps960025@gmail.com>
2024-12-06 22:05:01 +0800

    stock

--- testing/test1.py
+++ testing/test1.py
@@ -2,6 +2,7 @@ import requests
 import csv
 import os
 import sys
+import pandas as pd
 
 def get_exe_dir():
     """取得執行檔所在的目錄"""
@@ -12,7 +13,7 @@ def get_exe_dir():
 
 # 設定檔案路徑
 current_dir = get_exe_dir()
-file_path = os.path.join(current_dir, '每月營收.csv')
+file_path = os.path.join(current_dir, 'merged_sorted_data.csv')
 
 custom_fieldnames = {
     '公司代號': '公司代號',
@@ -32,15 +33,25 @@ custom_fieldnames = {
 }
 
 
-url = 'https://openapi.twse.com.tw/v1/opendata/t187ap05_L'
-data = requests.get(url)
-dj = data.json()
+url1 = 'https://openapi.twse.com.tw/v1/opendata/t187ap05_L' #上市
+url2 = 'https://www.tpex.org.tw/openapi/v1/mopsfin_t187ap05_O' #上櫃
+response1 = requests.get(url1)
+response2 = requests.get(url2)
+data1 = pd.DataFrame(response1.json())
+data2 = pd.DataFrame(response2.json())
+merged_data = pd.concat([data1, data2], ignore_index=True)
+
+# 根據項目編號遞增排序 (假設欄位名稱是 'item_id')
+sorted_data = merged_data.sort_values(by='公司代號')
+
+
+
 with open(file_path,'w',encoding='utf-8-sig',newline='') as f:
     fieldnames = ['公司代號','公司名稱','出表日期','營業收入-上月比較增減(%)','營業收入-上月營收','營業收入-去年同月增減(%)','營業收入-去年當月營收','營業收入-當月營收','產業別','累計營業收入-前期比較增減(%)','累計營業收入-去年累計營收','累計營業收入-當月累計營收','資料年月','備註']
     custom_fieldnames_list = list(custom_fieldnames.values())
     dict_writer = csv.DictWriter(f,fieldnames=custom_fieldnames_list)
     dict_writer.writeheader()
-    for row in dj:
-        # 根據自訂欄位名稱對應值
-        custom_row = {custom_fieldnames[key]: row[key] for key in fieldnames}
+    for _, row in sorted_data.iterrows():
+        # 根據自訂欄位名稱對應值，重新生成字典
+        custom_row = {custom_fieldnames[key]: row[key] for key in fieldnames if key in row}
         dict_writer.writerow(custom_row)
